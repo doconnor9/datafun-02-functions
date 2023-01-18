c08b5fa2de3590d2b08575b7bb18b8497cff5145
doconnor9 <doconnor9@yahoo.com>
2023-01-18 15:33:24 -0600

    initial update

--- user_math.py
+++ user_math.py
@@ -1,6 +1,6 @@
 """
-Always start with a docstring that describes what the module does.
-Include your name and the date.
+Module 2 Assignment by Diandra O'Connor 1/18/23
+Updated file with 3 functions related to gymnastics.
 
 Use built-in functions and 
 functions from the math module.
@@ -27,7 +27,7 @@ def get_area_of_lot(length, width):
     # Use a try / except / finally block when something 
     # could go wrong
     try: 
-        area = 0 # fix this
+        area = length * width # fix this
         return area
     except Exception as ex:
         print(f"Error: {ex}")
@@ -35,6 +35,24 @@ def get_area_of_lot(length, width):
 
 
 # define more functions here (see instuctions)
+
+# average beam score
+# a function for average scores on beam
+def gymnast1_avg_beam_score(*scores):
+    return sum(scores) / len(scores)
+
+# all around score
+# using fsum from the math module to show gymnast all-around score
+def gymnast_all_around_score(*scores):
+    return math.fsum(scores)
+
+# competition leotard cost
+# a function for determining cost of competition leotard with tax
+def competition_leo_with_tax(price):
+    return price * 1.08  # assuming a tax rate of 8%
+
+
+
 
 
 
@@ -49,4 +67,32 @@ def get_area_of_lot(length, width):
 if __name__ == "__main__":
 
     # call your functions here (see instructions)
-    print("your code here")
+    print("Explore some functions in the math module")
+    print()
+    print(f"math.comb(5,1) = {math.comb(5,1)}")
+    print(f"math.perm(5,1) = {math.perm(5,1)}")
+    print()
+
+print("The area of lot (6,2) is:", get_area_of_lot(6,2))
+print()
+print("The area of lot (12,12) is:", get_area_of_lot(12,12))
+print()
+print(f"The area of lot (10,12) is {get_area_of_lot(10,12)}")
+print()
+
+# call function for average beam score
+print("Gymnast1 has beam scores of 9.4, 9.55, 9.725, 8.85 and 9.6")
+gymnast1_beam_scores = [9.4, 9.55, 9.725, 8.85, 9.6]
+print(f"Gymnast1 average beam score is {gymnast1_avg_beam_score(9.4, 9.55, 9.725, 8.85, 9.6)}")
+print()
+
+# call function for all around score
+print("Gymnast2 has the following scores: vault 9.55, bars 8.8, beam 9.4, floor 9.675")
+print(f"Gymnast2 earned an all around score of {gymnast_all_around_score(9.55, 8.8, 9.4, 9.675):.3f}")
+print()
+
+# call function for competition leotard cost
+print("The gymnasts competition leotard price is $99.99 before taxes")
+print()
+print(f"The cost of the gymnasts competition leotard with tax is ${competition_leo_with_tax(99.99):.2f}")
+print()
---
+++ user_mathout.py
@@ -0,0 +1,26 @@
+"""
+Created by Diandra O'Connor 1/18/23
+
+Results from running script created in user_math.py
+
+math.comb(5,1) = 5
+math.perm(5,1) = 5
+
+The area of lot (6,2) is: 12
+
+The area of lot (12,12) is: 144
+
+The area of lot (10,12) is 120
+
+Gymnast1 has beam scores of 9.4, 9.55, 9.725, 8.85 and 9.6
+Gymnast1 average beam score is 9.425
+
+Gymnast2 has the following scores: vault 9.55, bars 8.8, beam 9.4, floor 9.675
+Gymnast2 earned an all around score of 37.425
+
+The gymnasts competition leotard price is $99.99 before taxes
+
+The cost of the gymnasts competition leotard with tax is $107.99
+
+
+"""
